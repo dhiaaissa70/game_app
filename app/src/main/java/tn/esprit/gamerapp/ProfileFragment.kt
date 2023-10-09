57f2bf8e995d0c5387a39b8edaf99d7cb7456b6f
dhiaaissa70 <dhia.aissa@esprit.tn>
2023-10-10 00:01:23 +0100

    second commit

---
+++ app/src/main/java/tn/esprit/gamerapp/ProfileFragment.kt
@@ -0,0 +1,11 @@
+package tn.esprit.gamerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_fragment)
+    }
+}
